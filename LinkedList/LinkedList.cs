3fafad899e4efb2ae9ae7daa84a9f0ca75f8f2b8
Heorhin, Dmytro <Dmytro.Heorhin@wolterskluwer.com>
2017-05-03 18:07:52 +0300

    Initial commit

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList<T> : IEnumerable, IEnumerator
+    {
+        private LinkedListNode<T> _head;
+        private LinkedListNode<T> _current;
+        
+        public int Length { get; private set; }
+        public LinkedListNode<T> Add(T item)
+        {
+            var result = new LinkedListNode<T>(this, item);
+
+            if (_head == null)
+            {
+                _head = result;
+                Length = 1;
+                return result;
+            }      
+
+            
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            if(_current.Next == null)
+            {
+                return false;
+            }
+            _current = _current.Next;
+            return true;
+        }
+
+        public void Reset()
+        {
+            _current = default();
+        }
+
+        private LinkedListNode<T> GetLast
+        {
+            
+        }
+
+        public object Current
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ LinkedList/LinkedListNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedListNode<T>
+    {
+        public LinkedListNode(LinkedList<T> list, T item)
+        {
+            List = list;
+            Item = item;
+        }
+
+        public LinkedList<T> List { get; internal set; }
+        public LinkedListNode<T> Next { get; internal set; }
+        public T Item { get; set; }      
+    }
+}
